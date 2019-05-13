1f508c1dfc6d258cbf5a51fedc22b5b4948971b4
AllanCh <edochacon96@gmail.com>
2019-05-12 22:06:08 -0600

    Actualizacion  archivo database.js

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');//Mongoose permite conectarse a mongodb y modelar datos
 
 const MONGODB_URI = 'mongodb://localhost/LibraryTecDB';//Coneccion a nivel local con la BD TECHealthBD 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true } )
     .then(db => console.log('DataBase connected'))
    .catch(err => console.error(err));
 module.exports = mongoose;
