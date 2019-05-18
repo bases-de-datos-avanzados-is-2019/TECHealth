165af4e0871c8a7798004f0bf875389e2f27e5fa
AllanCh <edochacon96@gmail.com>
2019-05-18 06:47:26 -0600

    heroku deploy (5)

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');//Mongoose permite conectarse a mongodb y m
 //Coneccion a nivel local con la BD 
 
 try {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true } )
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/librarytec', { useNewUrlParser: true } )
     console.log('Data Base connected');
 } catch (error) {
     console.error(error);
