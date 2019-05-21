930930549156aa68663d9ebd2d738b950050758f
AllanCh <edochacon96@gmail.com>
2019-05-20 22:30:18 -0600

    Se elimina get en login

--- backend/routes/logIn.routes.js
+++ backend/routes/logIn.routes.js
@@ -2,26 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 
-router.get('/:json', async (req, res) => {
-    const parametros = JSON.parse(req.params.json);
-    const {user, password} = parametros;
-      
-    const usuario = await User.findOne({nombreUsuario: user});
-    if(usuario === null){
-        res.json({id:-1, tipoUsuario:'Usuario no encontrado'});
-        return
-    }
-    if(usuario.comparePassword(password) === false){
-        res.json({id: -1, tipoUsuario:'Contraseña incorrecta'});
-        return
-    }
-        res.json({
-            id: usuario._id,
-            tipoUsuario: usuario.tipoUsuario
-        }
-        );    
-});
-
 router.post('/:json', async (req, res) => {
     const parametros = JSON.parse(req.params.json);
     const {user, password} = parametros;
