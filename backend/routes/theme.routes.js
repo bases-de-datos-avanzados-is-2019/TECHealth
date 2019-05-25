5ab25d7e207a22a33dca17c048fa75cabc948d74
gosaenz <gosaenz@hotmail.com>
2019-05-24 22:38:46 -0600

    Update theme.routes.js

--- backend/routes/theme.routes.js
+++ backend/routes/theme.routes.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();//Para poder crear las rutas de los recursos
 //El metodo res.send({Object}) es la respuesta que se le envia al navegador
 const Theme = require('../models/theme.model');
+const Book = require('../models/book.model');
 router.get('/', async (req, res) => {
     const Themes = await Theme.find();
     res.json(Themes);
 });
 
-router.get('/tema', async (req, res) => {
-    /*var temas = await Theme.find();
-    console.log(temas);
+router.get('/temas', async (req, res) => {
+    var temas = await Theme.find({}, {'_id':0, 'nombre': 1});
     const largo = temas.length;
     var result = { resultado: []};
     for (var i = 0; i < largo; i++){
-        var query = {tema: temas[i]};
+        var query = {tema: temas[i].nombre};
         var libros = await Book.find(query);
         var largolibros = libros.length;
         var vendidosTotales = 0;
@@ -22,10 +22,10 @@ router.get('/tema', async (req, res) => {
             vendidosTotales += libros[j].cantidadVendida;
             montoTotal += libros[j].precioDolares;
         };
-        var temp = {tema: temas[i], cantidadVendida: vendidosTotales, montoPromedio: montoTotal/largolibros};
+        var temp = {tema: temas[i].nombre, cantidadVendida: vendidosTotales, montoPromedio: montoTotal/largolibros};
         result.resultado.push(temp);
-    };*/
-    res.json({mensaje: "result"});
+    };
+    res.json(result);
     return;
 });
 
