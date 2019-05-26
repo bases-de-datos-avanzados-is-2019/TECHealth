bdcec460dab0ba1f6e3647630f7dc097bbee1bfc
AllanCh <edochacon96@gmail.com>
2019-05-26 14:07:49 -0600

    orden  post arreglada

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -14,8 +14,12 @@ router.get('/IdCliente/:id', async(req,res) => {
 });
 
 router.post('/:json', async (req, res) => {
+    var IdPedido = 1;
     const orders = await Order.find();
-    const IdPedido = orders[orders.length - 1].IdPedido + 1;
+    if(orders[0] !== undefined){
+        IdPedido = orders[orders.length - 1].IdPedido + 1;
+    }
+    
     const parametros = JSON.parse(req.params.json);
     const {IdCliente, libros, montoTotal, direccionEntrega} = parametros;
     const newOrder = new Order({IdPedido, IdCliente, libros, montoTotal, direccionEntrega});
